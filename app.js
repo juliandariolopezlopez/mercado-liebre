3260204d916a5a0dfb06f44b67fe35397d2100f4
JDDL.dev <juliandariolopeztulua@gmail.com>
2023-06-04 20:27:31 -0500

    Algunas mejoras en mercado liebre

--- app.js
+++ app.js
@@ -6,18 +6,15 @@ const app = express();
 
 const PORT= process.env.PORT || 3000;
 
-//const publicPath = path.resolve(__dirname, './public');
-//app.use(express.static(publicFolderPath));
-
-const publicFolderPath = path.resolve(__dirname, './public');
+const publicFolderPath = path.join(__dirname, './public');
 app.use(express.static(publicFolderPath));
 
 app.get('/', (req, res) => {
-    res.sendFile(path.resolve(__dirname, './views/home.html'));
+    res.sendFile(path.join(__dirname, './views/home.html'));
 })
  
 app.get('/', (res, req) => {
-    res.sendFile(path.resolve(__dirname, './css/styles.css'));
+    res.sendFile(path.join(__dirname, './css/styles.css'));
 })
 app.listen(PORT, () => {
     console.log('Servidor corriendo en el puerto' + PORT);
